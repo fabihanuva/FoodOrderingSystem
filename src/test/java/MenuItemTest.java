3bced300ccb023fd2f69be627998910112397a6b
fabihanuva <nuva.swe@gmail.com>
2026-06-27 22:38:26 +0600

    test: TC02 - Constructor sets price correctly

--- src/test/java/MenuItemTest.java
+++ src/test/java/MenuItemTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-import java.io.*;
 
 class MenuItemTest {
 
@@ -16,19 +15,5 @@ class MenuItemTest {
     void testConstructorSetsPrice() {
         MenuItem item = new MenuItem("Shingara", 10);
         assertEquals(10, item.price);
-    }
-
-    @Test
-    @DisplayName("TC03 - Constructor always initializes quantity to 0")
-    void testConstructorSetsQuantityToZero() {
-        MenuItem item = new MenuItem("Drinks", 25);
-        assertEquals(0, item.quantity);
-    }
-
-    @Test
-    @DisplayName("TC04 - Subtotal is 0 when quantity is 0 (default)")
-    void testSubtotalIsZeroByDefault() {
-        MenuItem item = new MenuItem("Eggchop", 10);
-        assertEquals(0, item.getSubtotal());
     }
 }
