a176702b7d6c5512a6f02b71242d208a51c24544
fabihanuva <nuva.swe@gmail.com>
2026-06-27 22:21:34 +0600

    test: TC01 - Constructor sets name correctly

---
+++ src/test/java/MenuItemTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+
+class MenuItemTest {
+
+    @Test
+    @DisplayName("TC01 - Constructor sets name correctly")
+    void testConstructorSetsName() {
+        MenuItem item = new MenuItem("Khichuri", 40);
+        assertEquals("Khichuri", item.name);
+    }
+
+    @Test
+    @DisplayName("TC02 - Constructor sets price correctly")
+    void testConstructorSetsPrice() {
+        MenuItem item = new MenuItem("Shingara", 10);
+        assertEquals(10, item.price);
+    }
+
+    @Test
+    @DisplayName("TC03 - Constructor always initializes quantity to 0")
+    void testConstructorSetsQuantityToZero() {
+        MenuItem item = new MenuItem("Drinks", 25);
+        assertEquals(0, item.quantity);
+    }
+
+    @Test
+    @DisplayName("TC04 - Subtotal is 0 when quantity is 0 (default)")
+    void testSubtotalIsZeroByDefault() {
+        MenuItem item = new MenuItem("Eggchop", 10);
+        assertEquals(0, item.getSubtotal());
+    }
+}
