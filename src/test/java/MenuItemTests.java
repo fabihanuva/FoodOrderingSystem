780007e517fc7b5436a9b9deadc8030e789a0961
Jannatul Ferdous Nishat <jannatulnsht@gmail.com>
2026-08-01 10:35:12 +0600

    Added MenuItem basic JUnit tests assertEquals and assertNotEquals

---
+++ src/test/java/MenuItemTests.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MenuItemTests {
+
+    MenuItem item;
+
+    @BeforeEach
+    void setUp() {
+        item = new MenuItem("Burger", 100);
+        System.out.println("Before Each Test");
+    }
+    @AfterEach
+    void tearDown() {
+        System.out.println("After Each Test");
+    }
+
+    @Test
+    void testAssertEquals() {
+
+        item.quantity = 2;
+
+        assertEquals(200, item.getSubtotal());
+    }
+
+    @Test
+    void testAssertNotEquals() {
+
+        item.quantity = 2;
+
+        assertNotEquals(100, item.getSubtotal());
+    }
+
+
+
+
+}
